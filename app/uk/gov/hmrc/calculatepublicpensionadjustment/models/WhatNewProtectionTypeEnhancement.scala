321d8c1c5b8c56ae465b15f12324361a97600207
Sandip Khedekar <17478517+SandipKhedekar@users.noreply.github.com>
2023-09-26 13:15:19 +0100

    MCSC-394 - LTA data model fix

--- app/uk/gov/hmrc/calculatepublicpensionadjustment/models/WhatNewProtectionTypeEnhancement.scala
+++ app/uk/gov/hmrc/calculatepublicpensionadjustment/models/WhatNewProtectionTypeEnhancement.scala
@@ -31,10 +31,6 @@ object WhatNewProtectionTypeEnhancement extends Enumerable.Implicits {
   case object IndividualProtection2016
       extends WithName(s"individualProtection2016")
       with WhatNewProtectionTypeEnhancement
-  case object InternationalEnhancement
-      extends WithName(s"internationalEnhancement")
-      with WhatNewProtectionTypeEnhancement
-  case object PensionCredit extends WithName(s"pensionCredit") with WhatNewProtectionTypeEnhancement
 
   val values: Seq[WhatNewProtectionTypeEnhancement] = Seq(
     EnhancedProtection,
@@ -43,9 +39,7 @@ object WhatNewProtectionTypeEnhancement extends Enumerable.Implicits {
     FixedProtection2014,
     FixedProtection2016,
     IndividualProtection2014,
-    IndividualProtection2016,
-    InternationalEnhancement,
-    PensionCredit
+    IndividualProtection2016
   )
 
   implicit val enumerable: Enumerable[WhatNewProtectionTypeEnhancement] =
