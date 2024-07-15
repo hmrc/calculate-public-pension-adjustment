7d5d1c26fe6723ede61e0c38c966a66d45ffbf0d
bharti5verma <68339135+bharti5verma@users.noreply.github.com>
2024-07-15 11:11:48 +0100

    MCSC-972 Type changed to Long from Int

--- app/uk/gov/hmrc/calculatepublicpensionadjustment/config/AppConfig.scala
+++ app/uk/gov/hmrc/calculatepublicpensionadjustment/config/AppConfig.scala
@@ -28,7 +28,7 @@ class AppConfig @Inject() (config: Configuration, servicesConfig: ServicesConfig
 
   lazy val paacServiceUrl: String = servicesConfig.baseUrl("pension-annual-allowance-calculator")
 
-  val ttlInDays: Int = servicesConfig.getInt("mongodb.ttlInDays")
+  val ttlInDays: Long = servicesConfig.getInt("mongodb.ttlInDays")
 
   val sppaBaseUrl: String =
     config.get[Service]("microservice.services.submit-public-pension-adjustment").baseUrl
