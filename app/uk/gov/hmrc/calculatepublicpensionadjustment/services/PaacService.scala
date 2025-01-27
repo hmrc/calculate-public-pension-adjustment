b087dd983fbb52c57cfc8cdb9b6cb219f50212bf
Sandip Khedekar <17478517+SandipKhedekar@users.noreply.github.com>
2025-01-27 16:20:58 +0000

    MCSC-128 - Fix for not exhaustive pattern match warnings

--- app/uk/gov/hmrc/calculatepublicpensionadjustment/services/PaacService.scala
+++ app/uk/gov/hmrc/calculatepublicpensionadjustment/services/PaacService.scala
@@ -18,6 +18,7 @@ package uk.gov.hmrc.calculatepublicpensionadjustment.services
 
 import com.google.inject.Inject
 import uk.gov.hmrc.calculatepublicpensionadjustment.connectors.PaacConnector
+import uk.gov.hmrc.calculatepublicpensionadjustment.exceptions.InvalidInputException
 import uk.gov.hmrc.calculatepublicpensionadjustment.logging.Logging
 import uk.gov.hmrc.calculatepublicpensionadjustment.models.IncomeSubJourney
 import uk.gov.hmrc.calculatepublicpensionadjustment.models.calculation.Income.BelowThreshold
@@ -444,6 +445,8 @@ class PaacService @Inject() (connector: PaacConnector)(implicit ec: ExecutionCon
           grossGiftAidAmount,
           rASContributionsAmount
         )
+
+      case _ => throw InvalidInputException(s"Invalid period, $period during calculateRevisedCharge")
     }
 
   @tailrec
@@ -581,6 +584,8 @@ class PaacService @Inject() (connector: PaacConnector)(implicit ec: ExecutionCon
         NonScottishTaxRate
           ._2023(personalAllowance)
           .getTaxRate(revisedNetIncome, grossGiftAidAmount, rASContributionsAmount)
+
+      case _ => throw InvalidInputException(s"Invalid period, $period during findTaxRate")
     }
 
   def findFreeAllowance(scottishTaxYears: List[Period], period: Period): Int =
@@ -616,6 +621,8 @@ class PaacService @Inject() (connector: PaacConnector)(implicit ec: ExecutionCon
       case (true, Period._2023) => ScottishTaxRateAfter2018._2023(0).freeAllowance
 
       case (false, Period._2023) => NonScottishTaxRate._2023(0).freeAllowance
+
+      case _ => throw InvalidInputException(s"Invalid period, $period while findFreeAllowance")
     }
 
   def calculateTotalAmounts(
@@ -926,6 +933,7 @@ class PaacService @Inject() (connector: PaacConnector)(implicit ec: ExecutionCon
           )
         )
 
+      case _ => throw InvalidInputException(s"Invalid taxyear encountered while building paacTaxYears")
     }
     PaacRequest(paacTaxYears, paacTaxYears.map(_.period).sorted.max)
   }
