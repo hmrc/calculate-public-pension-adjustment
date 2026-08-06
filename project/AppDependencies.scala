7a15a7a9616115aa65579e2160eac00ef6a39c74
andrewbrown22 <45789050+andrewbrown22@users.noreply.github.com>
2026-08-06 15:46:10 +0100

    DDCNL-12266 update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,16 +2,16 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.7.0"
-  private val hmrcMongoVersion = "2.12.0"
+  private val bootstrapVersion = "10.8.0"
+  private val hmrcMongoVersion = "2.13.0"
   private val taxyearVersion   = "6.0.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"    % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"           % hmrcMongoVersion,
-    "com.beachape"            %% "enumeratum-play-json"         % "1.9.7",
+    "com.beachape"            %% "enumeratum-play-json"         % "1.9.8",
     "org.typelevel"           %% "cats-core"                    % "2.13.0",
-    "uk.gov.hmrc"             %% "internal-auth-client-play-30" % "4.3.0",
+    "uk.gov.hmrc"             %% "internal-auth-client-play-30" % "4.4.0",
     "uk.gov.hmrc.objectstore" %% "object-store-client-play-30"  % "2.5.0",
     "org.apache.xmlgraphics"   % "fop"                          % "2.11",
     "uk.gov.hmrc"             %% "tax-year"                     % taxyearVersion,
@@ -27,7 +27,7 @@ object AppDependencies {
     "org.scalatestplus"   %% "scalacheck-1-17"         % "3.2.18.0"       % Test,
     "org.scalatestplus"   %% "mockito-4-11"            % "3.2.18.0"       % Test,
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-test-play-30" % hmrcMongoVersion % Test,
-    "org.apache.pdfbox"    % "pdfbox"                  % "3.0.7"          % Test
+    "org.apache.pdfbox"    % "pdfbox"                  % "3.0.8"          % Test
   )
 
   val itDependencies = Seq(
